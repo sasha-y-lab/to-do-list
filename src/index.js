7850ce06c724c2d0ed3a55a0372008a2b3508ece
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2025-05-05 20:08:23 -0400

    Set up files for project

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 // index.js
 
 import "./styles.css";
-import { homeload } from "./homeload.js";
+//import { homeload } from "./homeload.js";
 
-import { menuload } from "./menuload.js";
-
-import { contactload } from "./contactload.js";
-
-
-const content = document.querySelector("#content");
-
-const homeBtn = document.querySelector("#home-btn");
-
-const menuBtn = document.querySelector("#menu-btn");
-
-const contactBtn = document.querySelector("#contact-btn");
-
-homeload();
-
-homeBtn.addEventListener("click", () => {
-   content.replaceChildren();
-    homeload();
-});
-
-menuBtn.addEventListener("click", () => {
-    content.replaceChildren();
-    menuload();
-});
-
-contactBtn.addEventListener("click", () => {
-    content.replaceChildren();
-    contactload();
-});
